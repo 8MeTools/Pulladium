f8f80ffdce1d3fd5cdd81fe8090d261befca4e5b
8MeTools <217718000+8MeTools@users.noreply.github.com>
2026-05-17 20:18:33 +0900

    chore: cw compiler path in BuildPulsar scripts

--- BuildPulsar.bat
+++ BuildPulsar.bat
@@ -17,7 +17,7 @@ SET "PULSAR=.\PulsarEngine"
 
 :: Change this as necessary depending on where you put CodeWarrior
 :: This path should be fine for most Windows users doing a default installation
-SET CC="C:\Program Files (x86)\Freescale\CW for MPC55xx and MPC56xx 2.10\PowerPC_EABI_Tools\Command_Line_Tools\mwcceppc.exe"
+SET CC=".\cw\mwcceppc.exe"
 
 :: Riivolution Destination (change as necessary)
 SET "RIIVO="
@@ -57,3 +57,4 @@ if %ErrorLevel% equ 0 if NOT "!RIIVO!" == "" (
 
 :end
 ENDLOCAL
+pause
